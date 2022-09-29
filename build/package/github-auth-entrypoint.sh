e0648d144ea26b55409626efd46fb0be3eb65d0d
rsouza <rsouza@mathworks.com>
2022-09-29 14:45:23 -0400

    Use SSH

--- build/package/github-auth-entrypoint.sh
+++ build/package/github-auth-entrypoint.sh
@@ -6,12 +6,13 @@ set -e
 # in the image build cache. Docker mounts the secret as a file in /run/secrets.
 #
 # If the secret is present, read it into an environment variable.
-if [ -f /run/secrets/GITHUB_TOKEN ]; then
-    export GITHUB_TOKEN="${GITHUB_TOKEN:-$(cat /run/secrets/GITHUB_TOKEN)}"
+if [ -f /run/secrets/GH_TOKEN ]; then
+    export GH_TOKEN="${GH_TOKEN:-$(cat /run/secrets/GH_TOKEN)}"
 fi
 
 # If the token is present in an environment variable, use `gh` to authenticate.
-if [ -n "${GITHUB_TOKEN}" ]; then
+if [ -n "${GH_TOKEN}" ]; then
+    gh auth login
     gh auth setup-git
 fi
 
