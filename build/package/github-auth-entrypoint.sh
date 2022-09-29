aca52439f912a57cf29fd70335f803c8df0a55eb
rsouza <rsouza@mathworks.com>
2022-09-29 14:14:32 -0400

    Change GITHUB_ to GH_

--- build/package/github-auth-entrypoint.sh
+++ build/package/github-auth-entrypoint.sh
@@ -7,18 +7,12 @@ set -e
 #
 # If the secret is present, read it into an environment variable.
 if [ -f /run/secrets/GITHUB_TOKEN ]; then
-    echo 'Secrets file exists'
     export GITHUB_TOKEN="${GITHUB_TOKEN:-$(cat /run/secrets/GITHUB_TOKEN)}"
-else
-    echo 'Secrets file does not exist'
 fi
 
 # If the token is present in an environment variable, use `gh` to authenticate.
 if [ -n "${GITHUB_TOKEN}" ]; then
-    echo 'Running gh auth setup-git'
     gh auth setup-git
-else
-    echo 'Not running gh auth setup-git'
 fi
 
 exec "$@"
