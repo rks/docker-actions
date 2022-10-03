a87cf9fec77c603e545a01ded2aa7c2138512b51
rsouza <rsouza@mathworks.com>
2022-10-03 10:13:12 -0400

    Reverse script order

--- build/package/scripts/ssh-agent-entrypoint.sh
+++ build/package/scripts/ssh-agent-entrypoint.sh
@@ -6,7 +6,7 @@ SSH_AUTH_SOCK="${SSH_AUTH_SOCK:-/tmp/ssh-auth.sock}"
 
 # If the SSH agent is not running, start it.
 if [[ ! -S "${SSH_AUTH_SOCK}" ]]; then
-    [[ "${DEBUG}" == "1" ]] && echo "Starting SSH agent"
+    [[ "${DEBUG}" == "1" ]] && echo "[ssh-agent-entrypoint.sh] Starting SSH agent"
 
     eval $(ssh-agent -a ${SSH_AUTH_SOCK} -t 10m) > /dev/null
 
@@ -23,14 +23,13 @@ GH_SSH_KEY="${GH_SSH_KEY:-$(cat ${GH_SSH_KEY_FILE})}"
 # Could make an architecture check here.
 SSH_AUTH_SOCK_MOUNT="${SSH_AUTH_SOCK_MOUNT:-${SSH_AUTH_SOCK}}"
 
-[[ "${DEBUG}" == "1" ]] && echo "Running ${@}"
+[[ "${DEBUG}" == "1" ]] && echo "[ssh-agent-entrypoint.sh] Running ${@}"
 
-# Set a variable that eval-ed code can look for
-SSH_AGENT_ENTRYPOINT=on eval "${@}"
+eval "${@}"
 
 # If the SSH agent is running, and we started it, stop it.
 if [[ -S "${SSH_AUTH_SOCK}" ]] && [[ $__sshAgentStarted == 1 ]]; then
-    [[ "${DEBUG}" == "1" ]] && echo "Stopping SSH agent"
+    [[ "${DEBUG}" == "1" ]] && echo "[ssh-agent-entrypoint.sh] Stopping SSH agent"
 
     eval $(ssh-agent -k -a $SSH_AUTH_SOCK) > /dev/null
 fi
